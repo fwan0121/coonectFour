4840c83122077a2f86356cf3fc4b5977b89a7af2
Yen Chen Chen <yenchen@YendeMBP.lan>
2024-07-06 19:25:35 +1000

    remove parcel and edit folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const path = require('path')
-app.use('/', express.static(path.join(__dirname, 'dist')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 // app.get('/', (req, res) => {
 //   res.send('Hello world')
--- playwright.config.js
+++ playwright.config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 module.exports = {
     timeout: 60000,
     use: {
-      baseURL: process.env.BASE_URL || 'http://localhost:1234',
+      baseURL: process.env.BASE_URL || 'http://localhost:3000',
       headless: true,
       viewport: { width: 1280, height: 720 },
       actionTimeout: 0,
     },
-    testDir: 'src/tests/e2e',
+    testDir: 'public/src/tests/e2e',
   };
   
